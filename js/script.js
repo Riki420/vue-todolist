c4cae302bfde13a210a2dadf46e2baa81cd75d83
Riki420 <girottiriccardo1998@gmail.com>
2021-07-29 14:51:44 +0200

    v-for nel li per stampare la lista

--- js/script.js
+++ js/script.js
@@ -2,3 +2,20 @@ console.log('Ok: ' + Vue);
 
 Vue.config.devtools = true;
 
+//inizializzo vue
+
+const root = new Vue({
+    el:'#root',
+    data:{
+        tasks: [
+            'Comprare le cose',
+            'Pulire il rusco',
+            'Buttare la camera',
+            'Mangiare il fare',
+            'Lavare la spesa'
+        ]
+    },
+    methods:{
+        
+    }
+})
